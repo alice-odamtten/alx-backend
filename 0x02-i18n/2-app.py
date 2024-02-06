107eaccde189c8ba34c05f6bcaa3f3de930fb157
sakyibeaodamtten <sakyibeaodamtten@gmail.com>
2024-02-06 23:30:45 +0000

    babel decorator

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,10 +17,16 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    '''determine the best match with our supported languages'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     '''returns hello world'''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
