593c053e2365b676a1f5a69981ba507f5cfea784
sakyibeaodamtten <sakyibeaodamtten@gmail.com>
2024-02-07 00:23:16 +0000

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''setup a basic Flask app'''
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''a class config'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    '''determine the best match with our supported languages'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    '''returns hello world'''
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
