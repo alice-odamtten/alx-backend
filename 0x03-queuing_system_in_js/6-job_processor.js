56d9e60c87e970157a372b7e40e330d4dc2cbd56
alice-odamtten <119321981+alice-odamtten@users.noreply.github.com>
2024-03-27 18:42:47 -0700

    Delete 0x03-queuing_system_in_js directory

--- 0x03-queuing_system_in_js/6-job_processor.js
+++
@@ -1,7 +0,0 @@
-const kue = require("kue");
-const q = kue.createQueue();
-
-function sendNotification(phoneNumber, message) {console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)}
-q.process('push_notification_code', (job) => {
-    sendNotification(job.data.phoneNumber, job.data.message)
-})
